c7d309fa14a1cc3d291913fd713478b79a71f358
JulienJ <julienjnthn@gmail.com>
2018-10-30 22:34:31 +0100

    Mise à jour IHM

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  res.status(404).send("Sorry can't find that!")
 });
 
 // error handler
